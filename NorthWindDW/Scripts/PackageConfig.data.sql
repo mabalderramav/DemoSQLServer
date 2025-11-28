cca8f5a937fc45f90cfc414d720eb0f7637f887e
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-11-27 22:35:45 -0400

    Adds PostDep;loyment scripts

---
+++ NorthWindDW/Scripts/PackageConfig.data.sql
@@ -0,0 +1,49 @@
+﻿IF NOT EXISTS(SELECT  1
+              FROM [dbo].[PackageConfig]
+              WHERE [TableName] = 'Customer')
+    BEGIN
+        INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Customer', 0)
+    END
+GO
+IF NOT EXISTS(SELECT  1
+              FROM [dbo].[PackageConfig]
+              WHERE [TableName] = 'Employee')
+    BEGIN
+        INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Employee', 0)
+    END
+GO
+IF NOT EXISTS(SELECT  1
+              FROM [dbo].[PackageConfig]
+              WHERE [TableName] = 'Supplier')
+    BEGIN
+        INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Supplier', 0)
+    END
+GO
+IF NOT EXISTS(SELECT  1
+              FROM [dbo].[PackageConfig]
+              WHERE [TableName] = 'Product')
+    BEGIN
+        INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Product', 0)
+    END
+GO
+IF NOT EXISTS(SELECT  1
+              FROM [dbo].[PackageConfig]
+              WHERE [TableName] = 'ShipInfo')
+    BEGIN
+        INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('ShipInfo', 0)
+    END
+GO
+IF NOT EXISTS(SELECT  1
+              FROM [dbo].[PackageConfig]
+              WHERE [TableName] = 'Shipper')
+    BEGIN
+        INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Shipper', 0)
+    END
+GO
+IF NOT EXISTS(SELECT  1
+              FROM [dbo].[PackageConfig]
+              WHERE [TableName] = 'Orders')
+    BEGIN
+        INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Orders', 0)
+    END
+GO

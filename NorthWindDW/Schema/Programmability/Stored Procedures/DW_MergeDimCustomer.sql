e4fae55a64777ed65631a60b05fb73aa086ab38e
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-11-28 21:02:56 -0400

    adds DW_MergeDimCustomer and DW_MergeDimEmployee

---
+++ NorthWindDW/Schema/Programmability/Stored Procedures/DW_MergeDimCustomer.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimCustomer]
+AS
+BEGIN
+
+    UPDATE dc
+    SET [CompanyName] = sc.[CompanyName],
+        [ContactName] = sc.[ContactName],
+        [ContactTitle] = sc.[ContactTitle],
+        [Address] = sc.[Address],
+        [City] = sc.[City],
+        [Region] = sc.[Region],
+        [Country] = sc.[Country]
+    FROM [dbo].[DimCustomer]        dc
+    INNER JOIN [staging].[Customer] sc ON (dc.[CustomerSK]=sc.[CustomerSK])
+END
+GO
---
+++ NorthWindDW/Schema/Programmability/Stored Procedures/DW_MergeDimEmployee.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimEmployee]
+AS
+BEGIN
+
+    UPDATE dc
+    SET [FirstName] = se.[FirstName],
+        [LastName] = se.[LastName],
+        [Title] = se.[Title],
+        [TitleOfCourtesy] = se.[TitleOfCourtesy],
+        [BirthDate] = se.[BirthDate],
+        [HireDate] = se.[HireDate],
+        [Address] = se.[Address],
+        [City] = se.[City],
+        [Region] = se.[Region],
+        [Country] = se.[Country],
+        [PostalCode] = se.[PostalCode],
+        [ReportsTo] = se.[ReportsTo]
+    FROM [dbo].[DimEmployee]        de
+    INNER JOIN [staging].[Employee] se ON (de.[EmployeeSK]=se.[EmployeeSK])
+END
+GO

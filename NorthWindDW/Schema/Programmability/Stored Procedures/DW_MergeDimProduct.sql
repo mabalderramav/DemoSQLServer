0a85dee23bd0a5f31907f82adbf0fa39281906a4
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-11-28 21:12:41 -0400

    adds DW_MergeDimProduct and DW_MergeDimSupplier

---
+++ NorthWindDW/Schema/Programmability/Stored Procedures/DW_MergeDimProduct.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimProduct]
+AS
+BEGIN
+    UPDATE dc
+    SET [ProductName]      = sp.[ProductName],
+        [QuantityPerUnit]  = sp.[QuantityPerUnit],
+        [UnitPrice]        = sp.[UnitPrice],
+        [UnitsInStock]     = sp.[UnitsInStock],
+        [UnitsOnOrder]     = sp.[UnitsOnOrder],
+        [ReorderLevel]     = sp.[ReorderLevel],
+        [Discontinued]     = sp.[Discontinued],
+        [CategoryName]     = sp.[CategoryName] 
+    FROM [dbo].[DimProduct]        dp
+    INNER JOIN [staging].[Product] sp ON (dp.[ProductSK]=sp.[ProductSK])
+END
+GO
---
+++ NorthWindDW/Schema/Programmability/Stored Procedures/DW_MergeDimSupplier.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimSupplier]
+AS
+BEGIN
+    UPDATE dc
+    SET [CompanyName] = ss.[CompanyName],
+        [ContactTitle] = ss.[ContactTitle],
+        [Address] = ss.[Address],
+        [City] = ss.[City],
+        [Region] = ss.[Region],
+        [Country] = ss.[Country],
+        [PostalCode] = ss.[PostalCode]
+    FROM [dbo].[DimSupplier]        ds
+    INNER JOIN [staging].[Supplier] ss ON (ds.[SupplierSK]=ss.[SupplierSK])
+END
+GO

8b6409adf95d850847f3741391edf75d4165e896
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-11-28 21:22:28 -0400

    adds DW_MergeDimShipInfo and DW_MergeDimShipper

---
+++ NorthWindDW/Schema/Programmability/Stored Procedures/DW_MergeDimShipInfo.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimShipInfo]
+AS
+BEGIN
+    UPDATE dc
+    SET [Address]        = ss.[Address],
+        [City]           = ss.[City],
+        [Region]        = ss.[Region],
+        [Country]       = ss.[Country]
+    FROM [dbo].[DimShipInfo]        ds
+    INNER JOIN [staging].[ShipInfo] ss ON (ds.[ShipInfoSK]=ss.[ShipInfoSK])
+END
+GO
---
+++ NorthWindDW/Schema/Programmability/Stored Procedures/DW_MergeDimShipper.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimShipper]
+AS
+BEGIN
+    UPDATE dc
+    SET [CompanyName] = ss.[CompanyName]
+    FROM [dbo].[DimShipper]        ds
+    INNER JOIN [staging].[Shipper] ss ON (ds.[ShipperSK]=ss.[ShipperSK])
+END
+GO
